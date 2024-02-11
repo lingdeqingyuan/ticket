b480ebcb77f61c6dedb61e099be86438f7aaa307
ryuji <yrejgnl11@163.com>
2024-02-11 12:53:55 +0800

    chore: add Nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div>
       222
-      <UserButton />
+      <UserButton afterSignOutUrl="/"/>
+      <Button>click me</Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,7 +6,10 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
+  darkMode: "class",
+
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +19,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
