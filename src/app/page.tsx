cd82a3c513d9e0e4c1505298f4da4ffc5efe3ccd
ryuji <yrejgnl11@163.com>
2024-02-11 13:06:42 +0800

    switch dark & light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@nextui-org/react";
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <div>
-      222
+      <ThemeSwitcher />
       <UserButton afterSignOutUrl="/"/>
       <Button>click me</Button>
     </div>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,12 @@
-'use client'
+"use client";
 
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "next-themes";
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      {children}
+      <ThemeProvider attribute="class" defaultTheme="dark">{children}</ThemeProvider>
     </NextUIProvider>
-  )
+  );
 }
