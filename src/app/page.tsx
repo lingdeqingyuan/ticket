25e2d874b7a7b8fbfcbdaf076751247d47048060
ryuji <yrejgnl11@163.com>
2024-02-11 14:05:41 +0800

    feat: responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { UserButton } from "@clerk/nextjs";
-import { Button } from "@nextui-org/react";
+import { Button, Spacer } from "@nextui-org/react";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <div>
-      <ThemeSwitcher />
-      <UserButton afterSignOutUrl="/"/>
-      <Button>click me</Button>
+      <header className="w-full h-14">
+        <div className="fixed top-4 right-8 flex items-center justify-stretch">
+          <ThemeSwitcher />
+          <Spacer x={4}/>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </header>
+      <div className="flex items-center justify-center m-4">
+        <main className="flex flex-col items-center justify-center w-full border-x-2 sm:w-full md:w-3/4 lg:w-1/2">
+        </main>
+      </div>
     </div>
   );
 }
